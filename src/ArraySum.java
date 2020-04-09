f0b90cfe48b6bdaeb6431aa2c6c781b64f5ed121
B Kleinen <kleinen@infrastructure.de>
2020-04-09 13:18:03 +0200

    added main and tests for empty arrays

--- src/ArraySum.java
+++ src/ArraySum.java
@@ -18,4 +18,10 @@ public class ArraySum {
         if (i == a.length) return 0;
         return a[i] + sumRecursiveImpl(i+1, a);
     }
+    public static void main(String[] args){
+        System.out.println("die summe von {93,2,46,2,5,7} ist (155 wäre richtig) "+ new ArraySum().sumRecursive(new int[]{93,2,46,2,5,7}));
+        System.out.println("die summe von {93,2,46,2,5,7} ist (155 wäre richtig) "+ new ArraySum().sumIterative(new int[]{93,2,46,2,5,7}));
+        System.out.println("die summe von {} ist (0 wäre richtig) "+ new ArraySum().sumRecursive(new int[]{}));
+        System.out.println("die summe von {} ist (0 wäre richtig) "+ new ArraySum().sumIterative(new int[]{}));
+    }
 }
--- test/ArraySumTest.java
+++ test/ArraySumTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArraySumTest {
@@ -11,5 +13,13 @@ class ArraySumTest {
     @org.junit.jupiter.api.Test
     void sumRecursive() {
         assertEquals(27,sut.sumRecursive(new int[]{5,3,2,6,7,4}));
+    }
+    @Test
+    void sumEmptyIt(){
+        assertEquals(0,sut.sumIterative(new int[]{}));
+    }
+    @Test
+    void sumEmptyRec(){
+        assertEquals(0,sut.sumRecursive(new int[]{}));
     }
 }
